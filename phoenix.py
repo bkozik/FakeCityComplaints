3010a9cf8139d62113182a0d3802448ad8e864a6
Colton Sowers <cdsowers@asu.edu>
2018-11-11 13:50:48 -0700

    change from list to 1 input string

--- phoenix.py
+++ phoenix.py
@@ -3,11 +3,21 @@ import csv
 
 def get_complaints():
     complaint_list = []
+    complaintstr = ""
     with open('propertymaintenance.csv', encoding='utf8') as complaints:
         csv_reader = csv.reader(complaints)
 
+        i = 0
         for row in csv_reader:
             # 5 is the index of the comment in the report for each row
-            complaint_list.append(row[5])
+            complaintstr += row[5] + str(" ")
+            if i > 50:
+                break
+            i += 1
+            # complaint_list.append(row[5])
 
-    return complaint_list
+    with open('bigstring.txt', "w", encoding='utf8') as writefile:
+        writefile.write(complaintstr)
+
+
+get_complaints()
